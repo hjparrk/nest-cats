e827385ddfd02a40cae8c18aa751200506e2b820
Hyojoon Park <hpar9758@naver.com>
2024-04-05 16:30:51 +1100

    connect to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middlewares/logger/logger.middleware';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
